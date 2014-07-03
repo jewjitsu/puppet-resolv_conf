ce5f8a1f5008eb352ee15a78116dbfcfd60f2a1e
Seth Lyons <seth.lyons@gmail.com>
2014-07-03 12:32:53 -0400

    with pete #10

--- spec/classes/resolv_conf_spec.rb
+++ spec/classes/resolv_conf_spec.rb
@@ -65,7 +65,7 @@ describe 'resolv_conf' do
             expect {
               should compile
             }.to raise_error(Puppet::Error,
-                             "domainname and searchpath are mutually exclusive parameters")
+                             /domainname and searchpath are mutually exclusive parameters/)
           end
         end
       end
