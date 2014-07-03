f68193f948ff3684b0b3c67c49421515332ff853
Seth Lyons <seth.lyons@gmail.com>
2014-07-03 11:57:15 -0400

    with pete #1

--- spec/classes/resolv_conf_spec.rb
+++ spec/classes/resolv_conf_spec.rb
@@ -28,7 +28,7 @@ describe 'resolv_conf' do
       :options     => ['timeout:2', 'attempts:3'],
     },
   ].each do |param_set|
-    describe "when #{param_set == {} ? "using default" : "specifying"} class parameters" do
+    describe "when #{param_set == {} ? "setting searchpath and dominname" : "specifying"} class parameters" do
 
       let :param_hash do
         default_params.merge(param_set)
@@ -116,7 +116,7 @@ describe 'resolv_conf' do
       :options     => ['timeout:2', 'attempts:3'],
     },
   ].each do |param_set|
-    describe "when #{param_set == {} ? "using default" : "specifying"} class parameters" do
+    describe "when #{param_set == {} ? "setting searchpath without domainname" : "specifying"} class parameters" do
 
       let :param_hash do
         default_params.merge(param_set)
@@ -157,7 +157,7 @@ describe 'resolv_conf' do
               'content'
             )
             expected_lines = [
-              "search #{param_hash[:searchpath]}",
+#              "search #{param_hash[:searchpath]}",
             ]
 
             if(!param_hash[:searchpath].empty?)
@@ -184,7 +184,7 @@ describe 'resolv_conf' do
 
   let :default_params do
     {
-      :searchpath  => 'example.com',
+#      :searchpath  => 'example.com',
       :nameservers => ['192.168.0.1', '192.168.1.1', '192.168.2.1'],
       :domainname  => 'domain.tld',
     }
@@ -201,7 +201,7 @@ describe 'resolv_conf' do
       :options     => ['timeout:2', 'attempts:3'],
     },
   ].each do |param_set|
-    describe "when #{param_set == {} ? "using default" : "specifying"} class parameters" do
+    describe "when #{param_set == {} ? "when setting domainname without searchpath" : "specifying"} class parameters" do
 
       let :param_hash do
         default_params.merge(param_set)
@@ -267,4 +267,90 @@ describe 'resolv_conf' do
 
     end
   end
+
+  let :default_params do
+    {
+      :nameservers => ['192.168.0.1', '192.168.1.1', '192.168.2.1'],
+    }
+  end
+
+  [
+    {
+      :nameservers => ['192.168.0.1', '192.168.1.1', '192.168.2.1'],
+    },
+    {
+      :nameservers => ['192.168.0.1', '192.168.1.1', '192.168.2.1'],
+      :options     => ['timeout:2', 'attempts:3'],
+    },
+    {
+      :nameservers => ['192.168.0.1', '192.168.1.1', '192.168.2.1'],
+      :options     => ['timeout:2', 'attempts:3'],
+    },
+  ].each do |param_set|
+    describe "when #{param_set == {} ? "when setting neither searchpath nor domainname" : "specifying"} class parameters" do
+
+      let :param_hash do
+        default_params.merge(param_set)
+      end
+
+      let :params do
+        param_set
+      end
+
+      ['Debian', 'Redhat'].each do |osfamily|
+
+        let :facts do
+          {
+            :osfamily => osfamily,
+            :domain   => 'example.com',
+          }
+        end
+
+        describe "on supported osfamily: #{osfamily}" do
+
+          it { should contain_class('resolv_conf::params') }
+
+          it {
+            should contain_file('resolv.conf').with(
+              'ensure'  => 'file',
+              'path'    => '/etc/resolv.conf',
+              'owner'   => 'root',
+              'group'   => 'root',
+              'mode'    => '0644'
+            )
+          }
+
+          it 'should compile the template based on the class parameters' do
+            content = param_value(
+              subject,
+              'file',
+              'resolv.conf',
+              'content'
+            )
+            expected_lines = [
+              "domain #{param_hash[:domainname_real]}",
+            ]
+
+            if(!param_hash[:searchpath].empty?)
+              if(param_hash[:searchpath].kind_of?(Array))
+                expected_lines.push("search " + param_hash[:searchpath].join(" "))
+              else
+                expected_lines.push("search #{param_hash[:searchpath]}")
+              end
+            end
+
+            param_hash[:nameservers].each do |ns|
+                expected_lines.push("nameserver #{ns}")
+            end
+
+            if(param_hash[:options] && !param_hash[:options].empty?)
+              expected_lines.push("options " + param_hash[:options].join(" "))
+            end
+            (content.split("\n") & expected_lines).should =~ expected_lines
+          end
+        end
+      end
+    end
+  end
+
 end
